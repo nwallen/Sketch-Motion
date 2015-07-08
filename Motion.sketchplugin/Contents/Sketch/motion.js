18cf290a674e30fa63f83a8eec4e99cd137d9696
nwallen <nicholas.a.wallen@gmail.com>
2015-07-08 18:53:26 -0400

    Documented initial functionality

--- Motion.sketchplugin/Contents/Sketch/motion.js
+++ Motion.sketchplugin/Contents/Sketch/motion.js
@@ -1,13 +1,22 @@
- @import 'common.js'
- @import 'Tween.js'
+@import 'common.js'
+@import 'Tween.js'
 
- var testMotion = function(context) {
-    context.document.showMessage("running motion test");
-    var target = context.selection[0];
+var doc;
+var selection;
 
+var onStart = function(context){
     //TODO: resolve bug -- running script multiple times crashes Sketch
     [[COScript currentCOScript] setShouldKeepAround:true]
+    doc = context.document;
+    selection = context.selection;
+}
 
+// quick proof of concept test
+var testMotion = function(context) {
+    onStart(context);
+    doc.showMessage("running motion test");
+    
+    var target = context.selection[0];
     var animationComplete = false;
     var frame = [target rect];
     var tween = new TWEEN.Tween( { x: frame.origin.x, y: frame.origin.y } )
@@ -21,7 +30,7 @@
             view().refresh();
         })
         .onComplete( function(){
-            context.document.showMessage("done!");
+            doc.showMessage("done!");
             animationComplete = true;
         })
         .start();
@@ -34,4 +43,55 @@
     }];
 
  }
+
+var checkForAnimationReference = function(layerName) {
+    return layerName.match(/\{(\S+)\}/g)
+}
+
+var playAnimations = function(context){
+    onStart(context);
+    
+    var artboards = [];
+    for(var s=0; s < [selection count]; s++){
+        if(selection[s].isMemberOfClass(MSArtboardGroup)){
+            artboards.push(selection[s])
+        }
+    }
+    // requires user to select a single artboard
+    if (artboards.length !== 1){
+        doc.showMessage("please select one artboard")
+        return;
+    }
+    else {
+        doc.showMessage("animating...")
+         // Find animation(s) referenced in selected artboard
+        var artboardChildren = artboards[0].children();
+        for(var l=0; l < [artboardChildren count]; l++){
+             var child = artboardChildren.objectAtIndex(l);
+             var childName = child.name();
+             if(child.isMemberOfClass(MSLayerGroup)){
+                  var animationName = checkForAnimationReference(childName);
+                  if(animationName){
+                      log(animationName);
+                      // For each animation: 
+                      // Find related artboards (keyframes)
+                      // Order artboard keyframes
+                      
+                      // For each pair of artboard keyframes:
+                      // Detect differences in supported properties between layers with the same name
+                      // Save layer name and with all required tweens parameters to dictionary
+                      // Clear target group on selected artboard 
+                      // Copy in layers from initial artboard keyframe
+                  }
+             }
+        }
+    }
+   
+    
+    // When target artboard is initialized:
+    // Add tweens to update layers in selected artboard
+    // Start animation loop to run tweens
+    // On complete of last tween kill loop 
+}
+
 
---
+++ README.md
@@ -0,0 +1,11 @@
+#Sketch Motion Plugin
+
+FEATURES:
+-[X] Basic Tweened Animations Work In Sketch
+-[] Automatically Create and Run Position Animation 
+-[] Provide Basic Options to Adjust Animation Timing and Easing
+-[] Support all basic layer properties: Scale, Rotation, Opacity, Position
+-[] Spring Animations (rebound)
+-[] Support style properties: border, fill, shadow, inner shadow
+-[] Generate nice animation configuration artboard
+-[] Export GIF of animation(s) on selected artboard
